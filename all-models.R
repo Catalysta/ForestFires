4c8cd53931261b353d8ddbee17f786a7b565c861
FerJosafath <ferjosafath@outlook.com>
2019-04-14 10:04:43 -0400

    g

--- all-models.R
+++ all-models.R
@@ -10,7 +10,7 @@ fit = rpart(large ~ week+season+rain+
             dat=forestx,method="class",
             control = rpart.control(cp = 0.03))
 
-fit
+#fit
 plot(fit,margin = 0.1)
 text(fit,use.n=TRUE,cex=0.7)  # here cex will size the text labelling of nodes/leafs.
 
@@ -21,7 +21,7 @@ plotcp(fit)
 
 fit.pruned = prune(fit,cp=.03) 
 fit.pruned
-summary(fit.pruned)
+#summary(fit.pruned)
 plot(fit.pruned,margin = 0.1)
 text(fit.pruned,use.n=TRUE,cex=1.2)
 
@@ -53,4 +53,29 @@ for (i in 1:1023) {
     }
     
 }
+
+
+dev.min <- dev.min[,c(2,1)]
+
+g <- rbind(1:10,1:10)
+x.vec <- as.vector(g)
+x.vec[2*(1:10)] <- x.vec[2*(1:10)]+0.1
+x.vec[2*(1:10)-1] <- x.vec[2*(1:10)-1]-0.1
+x.vec
+
+devs <- as.vector(t(dev.min))
+x.vec <- x.vec[c(-1,-19)]
+devs <- devs[c(-1,-19)]
+
+plot(x.vec,devs,type = "l")
+points(x.vec,devs,pch=18)
+
+i = min[6,2]
+
+
+train.x <- train4[,c(as.integer(intToBits(i))[1:10]==1,T)]
+
+mylogit<-glm(formula = large ~.,
+             family = "binomial", data = train.x)
+summary(mylogit)
 
