f82b032c9e7480be7e64c3bb4193d87c3332b33c
Christina Sousa <christina.sousa85@gmail.com>
2019-04-21 10:50:02 -0400

    fixed interaction script

--- Christina_script.R
+++ Christina_script.R
@@ -112,6 +112,49 @@ for (i in 3:9){
   abline(lm(forest$Larea~X), col="blue", lwd=2)
 }
 
+#Generate Interaction Plots for continuous variables
+par(mfrow=c(2,3))
+for (i in 3:8){
+  for (j in (i+1):9){
+    y<-forest$Larea
+    Xr<-as.matrix(cbind(forest[,c(i,j)],forest[,i]*forest[,j]))
+    label1<-colnames(forest)[i]
+    label2<-colnames(forest)[j]
+    temp.model<-lm(y~Xr, data = forest)
+    
+    # Interaction
+    b0=temp.model$coefficients[1]
+    b1=temp.model$coefficients[2]
+    b2=temp.model$coefficients[3]
+    b3=temp.model$coefficients[4]
+    n=387
+    x1=Xr[,1]
+    x2=Xr[,2]
+    X=as.matrix(expand.grid(x1,x2))
+    yt=b0+b1*X[,1]+b2+X[,2]+b3*X[,1]*X[,2]
+    yp=b0+b1*X[,1]+b2+X[,2]
+    
+    # Let's look at the interaction in a lower-dimensional plot by conditioning
+    # on values of X1 and varying it.  
+    x1=c(mean(x1)-sd(x1),mean(x1),mean(x1)+sd(x1))
+    k=1
+    yt=(b0+b2*X[,2])+(b1+b3*X[,2])*x1[k]
+    plot(X[,2],yt,ylab="Predicted(Larea)", xlab = label2, ylim=c(0,2),
+         main=paste("Interaction Plot for", label1, " and", 
+                    label2),col=k,lwd=2,type='l')
+    for(k in 2:3)
+    {
+      yt=(b0+b2*X[,2])+(b1+b3*X[,2])*x1[k]
+      lines(X[,2],yt,col=k)
+    }
+    legend("topleft",col=c(1,2,3),legend=c(paste("-1SD ", label1),
+                                           paste("Mean ", label1),
+                                           paste("+1SD ", label1)),cex=1,lty=1)
+    Sys.sleep(1)
+  }
+}
+
+
 ############################MLR Analysis#################################
 #predictors that came out of EDA: TrFFMC, DMC, wind, temp, month
 #we first try MLR on these
@@ -139,7 +182,6 @@ par(mfrow=c(1,3))
 plot(regsubsets.out, scale = "r2", main = "R^2",cex.axis=2,cex.lab=2,cex=2)
 plot(regsubsets.out, scale = "adjr2", main = "Adjusted R^2",cex.axis=2,cex.lab=2,cex=2)
 plot(regsubsets.out, scale = "bic", main = "BIC",cex.axis=2,cex.lab=2,cex=2)
-
 
 reg.subsets.model<-lm(Larea~DMC+ISI+RH+wind+month, data=forest)
 summary(reg.subsets.model)
@@ -190,52 +232,93 @@ qqline(resids, col = "blue")
 #Other diagnostics
 #Durbin Watson test
 
-#Generate Interaction Plots
-par(mfrow=c(2,2))
-for (i in 3:8){
-  for (j in (i+1):9){
-    y<-forest$Larea
-    Xr<-as.matrix(cbind(forest[,c(i,j)],forest[,i]*forest[,j]))
-    label1<-colnames(forest)[i]
-    label2<-colnames(forest)[j]
-    temp.model<-lm(y~Xr, data = forest)
-    
-    # Interaction
-    b0=temp.model$coefficients[1]
-    b1=temp.model$coefficients[2]
-    b2=temp.model$coefficients[3]
-    b3=temp.model$coefficients[4]
-    n=387
-    x1=Xr[,1]
-    x2=Xr[,2]
-    X=as.matrix(expand.grid(x1,x2))
-    yt=b0+b1*X[,1]+b2+X[,2]+b3*X[,1]*X[,2]
-    yp=b0+b1*X[,1]+b2+X[,2]
-    
-    # Let's look at the interaction in a lower-dimensional plot by conditioning
-    # on values of X1 and varying it.  
-    x1=c(mean(x1)-sd(x1),mean(x1),mean(x1)+sd(x1))
-    i=1
-    yt=(b0+b2*X[,2])+(b1+b3*X[,2])*x1[i]
-    plot(X[,2],yt,ylab="Predicted(Larea)", xlab = label2, ylim=c(0,2),
-         main=paste("Interaction Plot for", label1, " and", 
-                    label2),col=i,lwd=2,type='l')
-    for(i in 2:3)
-    {
-      yt=(b0+b2*X[,2])+(b1+b3*X[,2])*x1[i]
-      lines(X[,2],yt,col=i)
-    }
-    legend("topleft",col=c(1,2,3),legend=c(paste("-1SD ", label1),
-                                           paste("Mean ", label1),
-                                           paste("+1SD ", label1)),cex=1,lty=1)
-  }
-}
+
+
+############################MLR Analysis with Interactions#################################
+#predictors that came out of EDA: TrFFMC, DMC, wind, temp, month
+#we first try MLR on these, including interaction terms suggested by the interaction plots
+
+MLR.eda.int.model<-lm(Larea~TrFFMC+DMC+wind+temp+month+TrFFMC:DMC+TrFFMC:temp+TrFFMC:wind+
+                    DMC:temp+DMC:wind+temp:wind, data = forest)
+summary(MLR.eda.int.model)
+
+#enumerate full model space
+require(leaps)
+attach(forest)
+y<-Larea
+X<-cbind(TrFFMC, DMC, DC, ISI, temp, RH, wind, month, day, TrFFMC*DMC, TrFFMC*temp, TrFFMC*wind,
+         DMC*temp,DMC*RH, DMC*wind, DC*temp, DC*RH, DC*wind, ISI*temp, ISI*wind, temp*wind, RH*wind)
+regsubsets.out=regsubsets(y ~ X,
+                          data = as.data.frame(cbind(y,X)),
+                          nbest = 2,       # 2 best models for each number of predictors
+                          nvmax = NULL,    # NULL for no limit on number of variables
+                          force.in = NULL, force.out = NULL,
+                          method = "exhaustive") 
+regsubsets.out
+sum.reg<-summary(regsubsets.out)
+sum.reg
+
+#plot regsubsets out, suggests we should drop TrFFMC, DC, and temp
+par(mfrow=c(1,3))
+plot(regsubsets.out, scale = "bic", main = "BIC",cex.axis=1,cex.lab=2,cex=2)
+
+#Create custom algorithm to condition on hierarchy
+init.model<-lm(Larea~1,data=forest[,-c(1,10)])
+for(i in 3:)
+
+#use forward selection, backward selection, and stepwise regression
+#to select a model #exclude spatial variables
+fit0<-lm(Larea~1,dat=forest)
+fitfull<-lm(Larea~.,dat=forest[,-c(1,9,11,15,16,17)])
+
+# Forward selection/Stepwise regression/Backwards Elim yield the same
+# Note per documentation, default criterion is BIC
+step(fit0,scope=list(lower=fit0,upper=fitfull),direction="forward") 
+
+fwd.model<-lm(Larea ~ DMC + month + temp + DC, data = forest)
+summary(fwd.model)
+
+BIC(MLR.eda.model, reg.subsets.model, fwd.model)
+
+#View diagnostics on the forward selection model
+par(mfrow=c(1,2))
+#Residual plot
+resids<-resid(fwd.model)
+fit.val<-fitted(fwd.model)
+plot(fit.val,resids, main = "Fitted Values vs. Residuals for 
+     Forward Selection Model")
+abline(h=0)
+#Q-Q Plot
+qqnorm(resids)
+qqline(resids, col = "blue")
+
+#Condition this model on nonzero burn area
+nz<-which(forest$area>0)
+fwd.nz.model<-lm(Larea ~ DMC + month + temp + DC, data = forest[nz,])
+summary(fwd.nz.model)
+
+#View diagnostics for this nonzero conditioned model
+par(mfrow=c(1,2))
+#Residual plot
+resids<-resid(fwd.nz.model)
+fit.val<-fitted(fwd.nz.model)
+plot(fit.val,resids, main = "Fitted Values vs. Residuals for 
+     Forward Selection Model")
+abline(h=0)
+#Q-Q Plot
+qqnorm(resids)
+qqline(resids, col = "blue")
+
+#Other diagnostics
+#Durbin Watson test
+
+
 
 
 
 #forward selection on interaction terms yielded
 fwd.intx.model<-lm(formula = Larea ~ ISI:temp:RH + DMC:ISI + TrFFMC:DMC:ISI + 
-                day, data = forest[nz,])
+                     day, data = forest[nz,])
 summary(fwd.intx.model)
 BIC(fwd.intx.model) # this actually does beat the existing model
 
@@ -253,7 +336,7 @@ qqline(resids, col = "blue")
 fwd.intx.h.model<-lm(formula = Larea ~ ISI+ temp + RH+ DC + TrFFMC+ 
                        ISI:temp + temp:RH + ISI:RH+ TrFFMC:ISI + DMC:ISI +
                        TrFFMC:ISI + ISI:temp:RH + DMC:ISI + TrFFMC:DMC:ISI + 
-                     day, data = forest[nz,])
+                       day, data = forest[nz,])
 summary(fwd.intx.h.model)
 BIC(fwd.intx.h.model) # this actually does beat the existing model
 
@@ -265,6 +348,7 @@ abline(h=0)
 #Q-Q Plot
 qqnorm(resids)
 qqline(resids, col = "blue")
+
 
 ############################PENALIZED REGRESSION##############################
 
